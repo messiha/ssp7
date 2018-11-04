bcd6715e4707df485c00db0ab0b5630e17cc4c0e
Aarsh Agarwal <19aarsh@gmail.com>
2018-11-04 08:09:40 +0530

    Implementation of IG completed

--- doa.m
+++ doa.m
@@ -2,7 +2,12 @@ clear all;
 %---------------------SETUP MODEL------------------------------------------
 %uniform linear array of 11 sensors with isotropic power kept at 0.5 lambda
 
-ula = phased.ULA('NumElements',11,'ElementSpacing',0.5);
+c = physconst('LightSpeed');
+fc = physconst('LightSpeed');              % Operating frequency
+lambda = c/fc;  %wavelength of 1 m
+uniform_distance = lambda/2;
+
+ula = phased.ULA('NumElements',11,'ElementSpacing',uniform_distance);
 
 %---------------------signal model-----------------------------------------
 ang1 = [-60; 0];          % First signal
@@ -21,10 +26,6 @@ angs = [ang1 ang2 ang3 ang4 ang5 ang6 ang7 ang8 ang9 ang10 ];
 ang = [ang1(1,1) ang2(1,1) ang3(1,1) ang4(1,1) ang5(1,1) ang6(1,1)...
     ang7(1,1) ang8(1,1) ang9(1,1) ang10(1,1)]/180*pi;
 
-
-c = physconst('LightSpeed');
-fc = physconst('LightSpeed');              % Operating frequency
-lambda = c/fc;  %wavelength of 1 m
 pos = getElementPosition(ula)/lambda; 
 
 Nsamp = 100;        % 100 samples
@@ -35,6 +36,7 @@ A = zeros(10,11);
 for k = 1:10
     A(k,:) = exp(-1i*2*pi*lambda/2*sin(ang(k))/lambda*[0:10]);
 end    
+A = transpose(A);
     
 rs = rng(1996);     % pseudo random states for reproducible results
 signal = sensorsig(pos,Nsamp,angs,nPower); %generated signal model samples
@@ -66,10 +68,46 @@ ymvdr = mvdrspatialspect(signal);
 ymusic = musicspatialspect(signal);
 helperPlotDOASpectra(mvdrspatialspect.ScanAngles,...
         musicspatialspect.ScanAngles,ymvdr,ymusic,'ULA')
+hold all;
 
 %----------------------IG Pencil-------------------------------------------
 
+signal = transpose(signal);
+
+%Since true covariance matrix is unavailable it is replaced by sample
+%covariance matrix
 R_hat = signal*signal';
+
+%eigen values and eigen vectors of sample covariance matrix
 [N,V] = eig(R_hat);
+
+%Linear Search for maximas from -pi/2 to pi/2
+phi = -90:1:90;
+yig = zeros(length(phi),1);
+for iter=1:length(phi)
+    a_phi = zeros(11,1);
+    for j = 1:11
+        a_phi(j,1) = exp(-1i*2*pi*(j-1)*uniform_distance/lambda*...
+            sin(phi(1,iter)/180*pi));
+    end
+    pencil = abs(a_phi'*inv(R_hat)*a_phi);
+    yig(iter,1) = 1/pencil;
+end
+
+plot(phi, yig);
+legend('MVDR','MUSIC','IG Pencil');
+
+
+
+    
+    
+        
+        
+    
+
+
+
+
+
 
 
