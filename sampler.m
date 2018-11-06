176f57113cb6f5570c7eab0f9e27a9d6837224de
harshv834 <harshv834@gmail.com>
2018-11-06 08:44:54 +0530

    Library used for STFT, STFT values for given input very low

--- sampler.m
+++ sampler.m
@@ -1,8 +1,8 @@
 clear;clc;
 %Source Motion Model Parameters
 delta_t = 0.375;
-N_samples = 1000;
-N_steps = int8(10/delta_t);
+N_particles = 1000;
+N_steps = int64(10/delta_t);
 beta = 2;
 v_bar = 1;
 a = exp(-beta*delta_t);
@@ -32,12 +32,11 @@ fs = 8000;
 c = 340;
 rt60  = 0.5;
 rir_samples = fs*delta_t;
-rir = zeros(N_steps+1,2,rir_samples);
 %rir(1,:,:) = rir_generator(c,fs,mic(1,:,:),[source(1,1:2) 0],room_dim,rt60,rir_samples);
 
 %Sampled Source Positions
-source_samp = zeros(N_steps+1,N_samples,4);
-source_samp(1,:,:) = [0.5*randi([3 9],N_samples,2) zeros(N_samples,2)];
+source_samp = zeros(N_steps+1,N_particles,4);
+source_samp(1,:,:) = [0.5*randi([3 9],N_particles,2) zeros(N_particles,2)];
 
 
 %Grid points
@@ -46,10 +45,16 @@ grid_pts = [X(:),Y(:)];
 kdeprob = zeros(N_steps,size(grid_pts,1));
 
 %Signal Array : sig 
+signal_raw = load('timit_audio.mat');
+signal_raw = signal_raw.audio_samps;
+%signal = reshape(signal_raw(1:(N_steps-1)*fs*delta_t), [N_steps-1 fs*delta_t]);
+%signal(N_steps,:) = reshape(signal_raw((N_steps-1)*fs*delta_t +1 :end),[1 (fs*10 -(N_steps-1)*fs*delta_t)]);
 
 %STFT params
-window_size = 100;
+window_size = 256;
+hop = window_size/4;
 window = rectwin(window_size);
+n_bins = 2^nextpow2(fs*delta_t);
 
 
 
@@ -57,34 +62,43 @@ for t = 1:N_steps
 	temp_source = mvnrnd(F*reshape(source(t,:),[4 1]),Q,1);
     a = temp_source(1:2) <0 ;
     b = temp_source(1:2) > 6;
-	while ((sum(a(:)) > 0) || (sum(b(:)) > 0))
-			temp_source = mvnrnd(F*reshape(source(t,:),[4 1])   ,Q,1);
+	%while ((sum(a(:)) > 0) || (sum(b(:)) > 0))
+	%	temp_source = mvnrnd(F*reshape(source(t,:),[4 1])   ,Q,1);
+    %end
+    
+	source(t+1,:) = temp_source;
+    if t == N_steps
+        rir = rir_generator(c,fs,reshape(mic(t,:,:),[2,3]),[source(t,1:2) 0],room_dim,rt60,fs*10 - fs*(N_steps-1)*delta_t);
+        S1 = conv(rir(:,1),signal_raw(fs*(N_steps-1)*delta_t+1:end));
+        S2 = conv(rir(:,2),signal_raw(fs*(N_steps-1)*delta_t+1:end));
+    else    
+        rir = rir_generator(c,fs,reshape(mic(t,:,:),[2,3]),[source(t,1:2) 0],room_dim,rt60,rir_samples);	
+        S1 = conv(rir(:,1),signal_raw((t-1)*fs*delta_t + 1: t*fs*delta_t));
+        S2 = conv(rir(:,2),signal_raw((t-1)*fs*delta_t + 1: t*fs*delta_t));
     end
     
-	source(t+1,:) = temp_source;		
-	rir(t,:,:) = rir_generator(c,fs,reshape(mic(t,:,:),[2,3]),[source(t,1:2) 0],room_dim,rt60,rir_samples);	
-	
-	%Convolve with signal from TIMIT database
-	%S1 = conv(rir(t,:,1),signal(t,:))
-	%S2 = conv(rir(t,:,2),signal(t,:))
-	%STFT with signal array
-	%Z(t,1,:) = spectrogram(S1,window,[],[],fs)
-	%Z(t,2,:) = spectrogram(S2,window,[],[],fs)
-
-	for j = 1:N_samples
-		
+    
+    %STFT with signal array
+	%Z1 and 
+    [Z1,farr1,tarr1] = stft(S1,window,hop,n_bins,fs);
+    [Z2,farr2,tarr2] = stft(S2,window,hop,n_bins,fs);
+    disp(sum(Z1));
+    disp(sum(Z2));
+   
+	for j = 1:N_particles
 		temp_samp = mvnrnd(F*reshape(source_samp(t,j,:),[4,1]),Q,1);
         a = temp_samp(:,1:2) < 0;
         b =  temp_samp(:,1:2) > 6;
-		while ( (sum(a(:)) > 0) || (sum(b(:)) > 0))
-			temp_samp = mvnrnd(F*reshape(source_samp(t,j,:),[4,1]),Q,1);
-        end
+		%while ( (sum(a(:)) > 0) || (sum(b(:)) > 0))
+		%	temp_samp = mvnrnd(F*reshape(source_samp(t,j,:),[4,1]),Q,1);
+        %end
    		source_samp(t+1,j,:) = temp_samp;
-		%Update equations for weight
-		%w(t+1,j,:) = w(t,j,:)
+	%Update equations for weight
+	%w(t+1,j,:) = w(t,j,:)
     end
     
 	%kdeprob(i,:),pts = ksdensity(reshape(source_samp(t,:,1:2),[N_samples,2]),grid_pts,'Weights',reshape(w(t,:,:),[N_samples,2]));
-end    
+end
+
 contour(X,Y,reshape(kdeprob(1,:),size(X)));
 		
